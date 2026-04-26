b793263fe714941888a0d517b7fde3dd776524e2
SHREYAS GOWDRU C S <shreyasgowdru8@gmail.com>
2026-04-27 00:59:54 +0530

    Update Hi.java

--- demo/src/main/java/com/example/Hi.java
+++ demo/src/main/java/com/example/Hi.java
@@ -1,1 +1,14 @@
 
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "HelloWorld!" );
+    }
+}
